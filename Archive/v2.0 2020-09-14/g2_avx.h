81ca97ea1a6baaed0e0b6c97203ebd7bb4133a6f
Ayman Wagih <aymanwmxyz@gmail.com>
2020-09-16 10:08:19 +0200

    License note

--- Archive/v2.0 2020-09-14/g2_avx.h
+++ Archive/v2.0 2020-09-14/g2_avx.h
@@ -1,3 +1,20 @@
+//g2_avx.h - All AVX functions are declared here.
+//All AVX functions are compiled in a separate project as a static library.
+//Copyright (C) 2012-2020  Ayman Wagih Mohsen
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef		AVX_H
 #define		AVX_H
 #include	"g2_common.h"
--- Archive/v2.0 2020-09-14/g2_graphics.h
+++ Archive/v2.0 2020-09-14/g2_graphics.h
@@ -1,3 +1,19 @@
+//g2_graphics.h - Include for G2 graphics API.
+//Copyright (C) 2012-2020  Ayman Wagih Mohsen
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include		"g2_common.h"
 #include		"g2_sse2.h"//lighten_sse2() for dim_screen()
 #include		<Windows.h>
